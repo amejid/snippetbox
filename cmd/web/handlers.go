c50c45b22fb9feedc84fe240d9337f302d71349e
amejid <abdelmejid89@gmail.com>
2024-12-17 15:28:48 +0300

    Restructure the code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("Hello from the other side"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
-
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
 
-	msg := fmt.Sprintf("Displaying snippet with ID %d", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Displaying snippet with ID %d", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -28,18 +26,6 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Creating"))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-	log.Print("Starting server on :4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
